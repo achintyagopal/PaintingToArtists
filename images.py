583a6e182e6abd2392250de479ec1d5e9595e676
billyonair <billyonair@acm.jhu.edu>
2016-12-02 20:40:29 -0500

    Added to BOW, added optional param in Images

--- images.py
+++ images.py
@@ -26,8 +26,8 @@ def save_image(img, filename):
     cv2.imwrite(filename, img)
 
 
-def show_image(img):
+def show_image(img, ms = 0):
     cv2.namedWindow("Window", cv2.WINDOW_NORMAL)
     cv2.imshow("Window", img)
-    cv2.waitKey(0)
+    cv2.waitKey(ms)
     cv2.destroyAllWindows()
