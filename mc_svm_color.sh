49d120f55e98e38ba8aaefd7ad84c898e397b645
billyonair <billyonair@acm.jhu.edu>
2016-12-08 01:51:52 -0500

    optimized bash

--- mc_svm_color.sh
+++ mc_svm_color.sh
@@ -12,12 +12,13 @@ printf "ALGORITHM: "
 printf $algorithm
 printf "\n"
 
+python main.py --mode feature --folder data --feature-file $feature.feature.file --feature-algorithm $feature
+
 for i in $(seq 0 $ITER);
 do
 	printf "Iteration: "
 	printf $i + 1
 	printf "\n"
-	python main.py --mode feature --folder data --feature-file $feature.feature.file --feature-algorithm $feature
 	python main.py --mode train --feature-file $feature.feature.file --training-algorithm $algorithm --model-file $algorithm.$feature.model.file
 	python main.py --mode test --feature-file $feature.feature.file --model-file $algorithm.$feature.model.file --predictions-file $algorithm.$feature.predictions
 done	
