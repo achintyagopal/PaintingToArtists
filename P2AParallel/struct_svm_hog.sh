653528a85c0b6d3cace00ad27270990a4c3b603c
billyonair <billyonair@acm.jhu.edu>
2017-05-08 14:38:48 -0400

    New Bash Script

--- P2AParallel/struct_svm_hog.sh
+++ P2AParallel/struct_svm_hog.sh
@@ -2,7 +2,7 @@
 
 ITER=20
 
-feature="color"
+feature="hog"
 algorithm="struct_svm"
 
 printf "FEATURE: "
@@ -12,11 +12,59 @@ printf "ALGORITHM: "
 printf $algorithm
 printf "\n"
 
+printf "1\n"
+python main.py --mode feature --folder ../data --procs 1 --feature-file $feature.feature.file --feature-algorithm $feature
+printf "2\n"
+python main.py --mode feature --folder ../data --procs 2 --feature-file $feature.feature.file --feature-algorithm $feature
+printf "4\n"
+python main.py --mode feature --folder ../data --procs 4 --feature-file $feature.feature.file --feature-algorithm $feature
+printf "8\n"
+python main.py --mode feature --folder ../data --procs 8 --feature-file $feature.feature.file --feature-algorithm $feature
+printf "16\n"
+python main.py --mode feature --folder ../data --procs 16 --feature-file $feature.feature.file --feature-algorithm $feature
 
+printf "\nPartition\n"
+
+printf "1\n"
+python main.py --mode feature --folder ../data --procs 8 --partition true --num_parts 1 --feature-file $feature.feature.file --feature-algorithm $feature
+printf "2\n"
+python main.py --mode feature --folder ../data --procs 8 --partition true --num_parts 2 --feature-file $feature.feature.file --feature-algorithm $feature
+printf "10\n"
+python main.py --mode feature --folder ../data --procs 8 --partition true --num_parts 10 --feature-file $feature.feature.file --feature-algorithm $feature
+printf "50\n"
+python main.py --mode feature --folder ../data --procs 8 --partition true --num_parts 50 --feature-file $feature.feature.file --feature-algorithm $feature
+printf "100\n"
+python main.py --mode feature --folder ../data --procs 8 --partition true --num_parts 100 --feature-file $feature.feature.file --feature-algorithm $feature
+
+
+printf "\nCVDATA\n"
+printf "1\n"
+python main.py --mode feature --folder ../cv --procs 1 --feature-file $feature.feature.file --feature-algorithm $feature
+printf "2\n"
+python main.py --mode feature --folder ../cv --procs 2 --feature-file $feature.feature.file --feature-algorithm $feature
+printf "4\n"
+python main.py --mode feature --folder ../cv --procs 4 --feature-file $feature.feature.file --feature-algorithm $feature
+printf "8\n"
+python main.py --mode feature --folder ../cv --procs 8 --feature-file $feature.feature.file --feature-algorithm $feature
+printf "16\n"
+python main.py --mode feature --folder ../cv --procs 16 --feature-file $feature.feature.file --feature-algorithm $feature
+
+printf "\nPartition\n"
+
+printf "1\n"
+python main.py --mode feature --folder ../cv --procs 8 --partition true --num_parts 1 --feature-file $feature.feature.file --feature-algorithm $feature
+printf "2\n"
+python main.py --mode feature --folder ../cv --procs 8 --partition true --num_parts 2 --feature-file $feature.feature.file --feature-algorithm $feature
+printf "10\n"
+python main.py --mode feature --folder ../cv --procs 8 --partition true --num_parts 10 --feature-file $feature.feature.file --feature-algorithm $feature
+printf "50\n"
+python main.py --mode feature --folder ../cv --procs 8 --partition true --num_parts 50 --feature-file $feature.feature.file --feature-algorithm $feature
+printf "100\n"
+python main.py --mode feature --folder ../cv --procs 8 --partition true --num_parts 100 --feature-file $feature.feature.file --feature-algorithm $feature
 
 # # NATIVE
 # python main.py --mode feature --folder ../data --procs 1 --feature-file $feature.feature.file --feature-algorithm $feature
-python main.py --mode feature --folder ../data --procs 3 --feature-file $feature.feature.file --feature-algorithm $feature
+# python main.py --mode feature --folder ../data --procs 3 --feature-file $feature.feature.file --feature-algorithm $feature
 # python main.py --mode feature --partition true --platform ipython --num_parts 10 --folder ../data --procs 3 --feature-file $feature.feature.file --feature-algorithm $feature
 # python main.py --mode feature --partition true --platform ipython --direct false --num_parts 10 --folder ../data --procs 3 --feature-file $feature.feature.file --feature-algorithm $feature
 # printf "pypy"
